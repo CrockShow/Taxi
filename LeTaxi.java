a4cb42161a49d7dc1af1e78c6e6840dd05dfa6e3
tekmor <thomas.dabre@bts-malraux.net>
2015-10-02 09:51:02 +0200

    SLAM 4 : Taxi version 2

--- LeTaxi.java
+++ LeTaxi.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public abstract class JoeLeTaxi {
+public abstract class LeTaxi {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
